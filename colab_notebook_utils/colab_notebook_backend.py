6405933acfb9478f13ea5216e085ccad04645974
jimmybuffi <jimmybuffi@gmail.com>
2023-04-22 16:28:35 -0700

    add doc strings and typing

--- colab_notebook_utils/colab_notebook_backend.py
+++ colab_notebook_utils/colab_notebook_backend.py
@@ -6,12 +6,18 @@ import os
 import pandas as pd
 import plotly
 
-from typing import Any, Dict, Generator, List, Optional
+from typing import Optional
+from collections.abc import Generator
 from . import utils as ut
 from .datatypes import Functions, InvocationTypes, S3Metadata, PlayerMetadata
 
 
-def display_available_df_data(df):
+def display_available_df_data(df: pd.DataFrame) -> None:
+    """
+    Display the data in each column of a pandas dataframe.
+
+    :param df: the dataframe to be displayed
+    """
     print('Available data...')
     print()
 
@@ -22,6 +28,12 @@ def display_available_df_data(df):
 
 
 def download_s3_summary_df(s3_metadata: S3Metadata) -> pd.DataFrame:
+    """
+    Download the CSV that summarizes all the current data in S3 into a dataframe.
+
+    :param s3_metadata: the S3Metadata object to use to download the S3 summary CSV
+    :return: dataframe of all the data in S3
+    """
 
     s3_summary_df = wr.s3.read_csv(
         [f"s3://reboot-motion-{s3_metadata.org_id}/population/s3_summary.csv"], index_col=[0]
@@ -40,13 +52,30 @@ def download_s3_summary_df(s3_metadata: S3Metadata) -> pd.DataFrame:
     return s3_summary_df
 
 
-def add_to_mask(mask, df, col, vals):
+def add_to_mask(mask: pd.Series, df: pd.DataFrame, col: str, vals: list) -> pd.Series:
+    """
+    Add more criteria to the mask for a pandas dataframe based filtering where values are in a column.
+
+    :param mask: the existing pandas mask to add to
+    :param df: the pandas dataframe to mask
+    :param col: the col to filter
+    :param vals: the rows of the dataframe to return contain these values
+    :return: a mask for a pandas dataframe
+    """
     return mask & df[col].isin(vals)
 
 
 def filter_s3_summary_df(player_metadata: PlayerMetadata, s3_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Filter the S3 summary dataframe for only rows that are associated with the input player metadata
+
+    :param player_metadata: the metadata to use to filter the dataframe
+    :param s3_df: the s3 summary dataframe
+    :return: the filtered dataframe that only includes rows related to the player metadata
+    """
+
     mask = (s3_df['mocap_type'] == player_metadata.s3_metadata.mocap_type) \
-           & (s3_df['movement_type'] == player_metadata.s3_metadata.movement_type)
+        & (s3_df['movement_type'] == player_metadata.s3_metadata.movement_type)
 
     if player_metadata.mlbam_player_ids:
         mask = add_to_mask(mask, s3_df, 'org_player_id', player_metadata.mlbam_player_ids)
@@ -73,20 +102,28 @@ def filter_s3_summary_df(player_metadata: PlayerMetadata, s3_df: pd.DataFrame) -
     return return_df
 
 
-def list_available_s3_keys(org_id, primary_df):
+def list_available_s3_keys(org_id: str, df: pd.DataFrame) -> list[str]:
+    """
+    List all files associated with a dataframe that contains a column of 's3_path_delivery' values.
+
+    :param org_id: the org ID to use in the delivery path
+    :param df: the dataframe with a column of s3_path_delivery values
+    :return: list of all available s3 file paths
+    """
     s3_client = boto3.session.Session().client("s3")
 
     bucket = f"reboot-motion-{org_id}"
 
     all_files = []
 
-    for s3_path_delivery in primary_df['s3_path_delivery']:
-        print('s3 base path', s3_path_delivery)
+    for s3_path_delivery in df['s3_path_delivery']:
+        print('s3 base path:', s3_path_delivery)
 
         key_prefix = '/'.join(s3_path_delivery.split('s3://')[-1].split('/')[1:])
         objs = s3_client.list_objects_v2(Bucket=bucket, Prefix=key_prefix)
 
         s3_files = sorted([obj['Key'] for obj in objs.get('Contents', [])])
+        print('available s3 files:')
         print(s3_files)
         print()
 
@@ -96,7 +133,12 @@ def list_available_s3_keys(org_id, primary_df):
 
 
 def load_games_to_df_from_s3_paths(game_paths: list[str]) -> pd.DataFrame:
+    """
+    For a list of paths to the S3 folder of data for each game, load the data into a pandas dataframe.
 
+    :param game_paths: list of paths to folders with data for a player
+    :return: dataframe of all data from all games
+    """
     game_paths = sorted(list(set(game_paths)))
 
     all_games = []
@@ -134,6 +176,16 @@ def load_data_into_analysis_dict(
         df_std: Optional[pd.DataFrame] = None,
         segment_label: str = None
 ) -> dict:
+    """
+    Load data from multiple games in a summary dict for further analysis.
+
+    :param player_metadata: the metadata used to create the analysis
+    :param df: dataframe across multiple games, will be filtered to one game based on the play ID in the metadata
+    :param df_mean: the desired mean dataframe, will be calculated if not provided
+    :param df_std: the desired standard deviation dataframe, will be calculated if not provided
+    :param segment_label: the desired label to use for the analysis segment
+    :return: dict of data to be used for this segment of an analysis
+    """
     print('Loading into dict player metadata:', player_metadata)
 
     analysis_dict = {
@@ -176,16 +228,16 @@ def load_data_into_analysis_dict(
     return analysis_dict
 
 
-def list_chunks(lst: List[Any], n: int) -> Generator[List[Any], None, None]:
+def list_chunks(lst: list, n: int) -> Generator[list, None, None]:
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
-        yield lst[i : i + n]
+        yield lst[i: i + n]
 
 
 def get_relative_frame(time_series: pd.Series) -> pd.Series:
     """Input a pandas series of floats, and return ranks with 0 as rank = 0 and negative numbers as negative ranks."""
     all_ranks = (
-        time_series.where(time_series >= 0).rank(method="first") - 1
+            time_series.where(time_series >= 0).rank(method="first") - 1
     )  # start at 0 instead of 1
     negative_ranks = time_series.where(time_series < 0).rank(
         method="first", ascending=False
@@ -194,7 +246,8 @@ def get_relative_frame(time_series: pd.Series) -> pd.Series:
     return all_ranks
 
 
-def get_available_joint_angles(analysis_dicts: List[Dict]) -> List[str]:
+def get_available_joint_angles(analysis_dicts: list[dict]) -> list[str]:
+    """Get available joint angle columns from a list of analysis dicts."""
     non_angle_col_prefixes = (
         "optim_",
         "time",
@@ -215,30 +268,54 @@ def get_available_joint_angles(analysis_dicts: List[Dict]) -> List[str]:
 
 
 def filter_pop_df(
-    mean_df: pd.DataFrame,
-    std_df: pd.DataFrame,
-    time_col: str,
-    angle_cols: List[str],
-    downsample: int = 1,
+        mean_df: pd.DataFrame,
+        std_df: pd.DataFrame,
+        time_col: str,
+        angle_cols: list[str],
+        downsample: int = 1,
 ) -> pd.DataFrame:
-    assert len(mean_df) == len(
-        std_df
-    ), "Mismatched frames between mean and std population data"
+    """
+    Filter a population dataframe and combine the mean and std dataframes.
+
+    :param mean_df: the population mean dataframe
+    :param std_df: the population standard deviation dataframe
+    :param time_col: the column to use for time
+    :param angle_cols: the joint angle columns to use
+    :param downsample: how many frames to skip when down sampling the dataframe
+    :return: the filtered and down-sampled population dataframe
+    """
+
+    assert len(mean_df) == len(std_df), "Mismatched frames between mean and std population data"
+
     df = mean_df[[time_col, *angle_cols]].copy()
+
     for angle_col in angle_cols:
         df[f"{angle_col}_std"] = std_df[angle_col].copy()
+
     df = df.iloc[::downsample, :]
+
     return df
 
 
 def filter_analysis_dicts(
-    analysis_dicts: List[Dict],
-    time_col: str,
-    angle_cols: List[str],
-    keep_df: bool = True,
-    keep_df_mean: bool = True,
-    downsample: int = 1,
-) -> List[Dict]:
+        analysis_dicts: list[dict],
+        time_col: str,
+        angle_cols: list[str],
+        keep_df: bool = True,
+        keep_df_mean: bool = True,
+        downsample: int = 1,
+) -> list[dict]:
+    """
+    Filter an analysis dict so that it is a smaller size for sending to AWS for analysis.
+
+    :param analysis_dicts: list of segment dicts to analyze
+    :param time_col: column to use for time in the analysis
+    :param angle_cols: list of joint angle columns to analyze
+    :param keep_df: whether to keep the 'df' key in the analysis dict
+    :param keep_df_mean: whether to keep the 'df_mean' in the analysis dict
+    :param downsample: how may frames to skip when down-sampling
+    :return: list of filtered analysis dicts
+    """
     joints = [
         "NECK",
         "LSJC",
@@ -254,11 +331,15 @@ def filter_analysis_dicts(
         "LAJC",
         "RAJC",
     ]
+
     keys_to_keep = {"mlb_play_guid", "mlbam_player_id"}
+
     if keep_df:
         keys_to_keep.add("df")
+
     if keep_df_mean:
         keys_to_keep.add("df_mean")
+
     df_cols_to_keep = {
         time_col,
         "time_from_max_hand",
@@ -267,15 +348,19 @@ def filter_analysis_dicts(
         "torso_rot",
         *angle_cols,
     }
+
     df_cols_to_keep.update(
         [f"{joint}_{dim}" for joint in joints for dim in ["X", "Y", "Z"]]
     )
 
     res = []
+
     for analysis_dict in analysis_dicts:
+
         filt_analysis_dict = {
             k: v for k, v in analysis_dict.items() if k in keys_to_keep
         }
+
         if keep_df:
             filt_analysis_dict["df"] = (
                 filt_analysis_dict["df"]
@@ -283,6 +368,7 @@ def filter_analysis_dicts(
                 .astype(np.float16, errors="ignore")
                 .iloc[::downsample, :]
             )
+
         if keep_df_mean:
             filt_analysis_dict["df_mean"] = (
                 filt_analysis_dict["df_mean"]
@@ -290,21 +376,67 @@ def filter_analysis_dicts(
                 .astype(np.float16)
                 .iloc[::downsample, :]
             )
+
         res.append(filt_analysis_dict)
+
     return res
 
 
+def handle_lambda_invocation(session: boto3.session.Session, payload: dict) -> str:
+    """
+    Invoke a lambda function with the input payload.
+
+    :param session: the boto3 session info to use
+    :param payload: the lambda payload
+    :return: the serialized lambda response
+    """
+    payload = json.dumps(payload, default=ut.serialize)
+
+    print('Sending to AWS...')
+    response = ut.invoke_lambda(
+        session=session,
+        lambda_function_name=Functions.VISUALIZATIONS,
+        invocation_type=InvocationTypes.SYNC,
+        lambda_payload=payload,
+    )
+
+    print('Reading Response...')
+    payload = response["Payload"].read()
+
+    if ut.lambda_has_error(response):
+        print(f"Error in calculation")
+        print(payload)
+        return payload
+
+    print("Returning Plotly figure...")
+    return plotly.io.from_json(json.loads(payload))
+
+
 def get_animation(
-    session: boto3.session.Session,
-    analysis_dicts: List[Dict[Any, Any]],
-    pop_mean_df: pd.DataFrame,
-    pop_std_df: pd.DataFrame,
-    time_column: str,
-    joint_angle: str,
-    plot_joint_angle_mean: bool,
-    frame_step: int = 25,
-    downsample_data: int = 2,
+        session: boto3.session.Session,
+        analysis_dicts: list[dict],
+        pop_mean_df: pd.DataFrame,
+        pop_std_df: pd.DataFrame,
+        time_column: str,
+        joint_angle: str,
+        plot_joint_angle_mean: bool,
+        frame_step: int = 25,
+        downsample_data: int = 2,
 ) -> str:
+    """
+    Use the input data to retrieve the synchronized skeleton and joint angle animation from AWS.
+
+    :param session: the input boto3 session
+    :param analysis_dicts: list of analysis dicts to animate
+    :param pop_mean_df: the population mean dataframe
+    :param pop_std_df: the population standard deviation dataframe
+    :param time_column: the column to use as the time
+    :param joint_angle: the joint angle to plot
+    :param plot_joint_angle_mean: whether to plot the joint angle mean or the joint angle for the specific pitch
+    :param frame_step: the number of frames between animation time points
+    :param downsample_data: by how much to downsample the data
+    :return: the serialized plotly animation figure
+    """
     times = analysis_dicts[0]["df"][time_column].tolist()[:: int(frame_step)]
 
     pop_df = filter_pop_df(
@@ -336,43 +468,38 @@ def get_animation(
 
     payload = {"function_name": "get_joint_angle_animation", "args": args}
 
-    payload = json.dumps(payload, default=ut.serialize)
-
-    print('Sending to AWS...')
-    response = ut.invoke_lambda(
-        session=session,
-        lambda_function_name=Functions.VISUALIZATIONS,
-        invocation_type=InvocationTypes.SYNC,
-        lambda_payload=payload,
-    )
-
-    print('Reading Response...')
-    payload = response["Payload"].read()
-    if ut.lambda_has_error(response):
-        print(f"Error in calculation")
-        print(payload)
-        return payload
-
-    print("Returning Plotly figure...")
-    return plotly.io.from_json(json.loads(payload))
+    return handle_lambda_invocation(session, payload)
 
 
 def get_joint_plot(
-    session: boto3.session.Session,
-    analysis_dicts: List[Dict[Any, Any]],
-    pop_mean_df: pd.DataFrame,
-    pop_std_df: pd.DataFrame,
-    time_column: str,
-    joint_angles: List[str],
-    plot_colors: List[str] = [
-        "rgb(31, 119, 180)",
-        "rgb(255, 127, 14)",
-        "rgb(44, 160, 44)",
-        "rgb(214, 39, 40)",
-        "rgb(148, 103, 189)",
-    ],
-    downsample_data: int = 2,
+        session: boto3.session.Session,
+        analysis_dicts: list[dict],
+        pop_mean_df: pd.DataFrame,
+        pop_std_df: pd.DataFrame,
+        time_column: str,
+        joint_angles: list[str],
+        plot_colors: list[str] = (
+            "rgb(31, 119, 180)",
+            "rgb(255, 127, 14)",
+            "rgb(44, 160, 44)",
+            "rgb(214, 39, 40)",
+            "rgb(148, 103, 189)",
+        ),
+        downsample_data: int = 2,
 ) -> str:
+    """
+    Plot a selection of joint angles on a plotly 2d scatter plot.
+
+    :param session: the input boto3 session
+    :param analysis_dicts: list of analysis dicts to animate
+    :param pop_mean_df: the population mean dataframe
+    :param pop_std_df: the population standard deviation dataframe
+    :param time_column: the column to use as the time
+    :param joint_angles: the list of joint angles to plot
+    :param plot_colors: the colors to use in the plot
+    :param downsample_data: by how much to downsample the input data
+    :return: the serialized 2d plotly joint plot
+    """
     pop_df = filter_pop_df(
         pop_mean_df,
         pop_std_df,
@@ -400,34 +527,23 @@ def get_joint_plot(
 
     payload = {"function_name": "get_joint_angle_plots", "args": args}
 
-    payload = json.dumps(payload, default=ut.serialize)
-
-    print('Sending to AWS...')
-    response = ut.invoke_lambda(
-        session=session,
-        lambda_function_name=Functions.VISUALIZATIONS,
-        invocation_type=InvocationTypes.SYNC,
-        lambda_payload=payload,
-    )
-    print('Reading Response...')
-    payload = response["Payload"].read()
-
-    if ut.lambda_has_error(response):
-        print(f"Error in calculation")
-        print(payload)
-        return payload
-
-    print("Returning Plotly figure...")
-    return plotly.io.from_json(json.loads(payload))
+    return handle_lambda_invocation(session, payload)
 
 
 def save_figs_to_html(
-    figs: List[plotly.graph_objects.Figure],
-    output_report_name: str = "report.html",
+        figs: list[plotly.graph_objects.Figure],
+        output_report_name: str = "report.html",
 ) -> None:
+    """
+    Save a list of plotly figures to an html report.
+
+    :param figs: the list of plotly figures to put into the report.
+    :param output_report_name: the output name of the report.
+    """
     if os.path.exists(output_report_name):
         os.remove(output_report_name)
         print("Removed old report")
+
     html_header = """
         <html><head>
         <title>Reboot Motion, Inc.</title>
@@ -436,6 +552,7 @@ def save_figs_to_html(
         <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
         </head><body>
     """
+
     html_footer = (
         "<div width='100%' align='left' style=\"padding:20px 0 20px 0; font-size: 12px; font-family: Verdana, sans-serif;\"> "
         "DISCLAIMER<br><br>This report is for general informational purposes only, it does not constitute the practice of medicine, "
@@ -450,11 +567,16 @@ def save_figs_to_html(
         "that is directly or indirectly related to the purchase and/or use of our information, products and services."
         "<br><br>Copyright Reboot Motion, Inc 2023</div></body></html>"
     )
+
     with open(output_report_name, "a+") as f:
+
         f.write(html_header)
+
         for fig in figs:
+
             if fig is None:
                 continue
+
             f.write(
                 fig.to_html(
                     full_html=False,
@@ -464,6 +586,8 @@ def save_figs_to_html(
                 )
             )
         f.write(html_footer.encode("utf-8").decode("utf-8"))
+
     print(
-        f"Now you can download the new {output_report_name} file from the files tab in the left side bar (refresh the list and click the three dots)"
+        f"Now you can download the new {output_report_name} "
+        f"file from the files tab in the left side bar (refresh the list and click the three dots)"
     )
