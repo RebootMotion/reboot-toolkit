31c9fa3071714c940a82e042929f62ce04a8e301
Robert Calise <rcalise@rebootmotion.com>
2023-08-10 11:42:41 -0400

    Fix var name

--- reboot_toolkit/utils.py
+++ reboot_toolkit/utils.py
@@ -42,7 +42,7 @@ def setup_aws(
     boto3_session = boto3.Session(
         aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        aws_session_token=os.environ['AWS_SECRET_ACCESS_KEY'],
+        aws_session_token=os.environ['AWS_SESSION_TOKEN'],
         region_name=os.environ['AWS_DEFAULT_REGION'],
     )
 
