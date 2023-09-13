21c9264226078fe7d2bdc4ccd4ba3c3834851d9b
jimmybuffi <jimmybuffi@gmail.com>
2023-09-12 20:13:00 -0700

    fix credentials

--- reboot_toolkit/utils.py
+++ reboot_toolkit/utils.py
@@ -16,32 +16,33 @@ from mlb_statsapi import GameRequest, Game
 logger = logging.getLogger(__name__)
 
 
-def setup_aws(
-        org_id: Optional[str] = None, 
-        aws_access_key_id: Optional[str] = None, 
-        aws_secret_access_key: Optional[str] = None, 
-        aws_default_region: Optional[str] = None
-    ) -> boto3.Session:
+def setup_aws() -> boto3.Session:
+
     load_dotenv()
 
     if 'ORG_ID' not in os.environ:
-        input_org_id = getpass(f'Input org_id here (or input empty string to use {org_id}):')
-        if len(input_org_id.strip()) == 0:
-            os.environ['ORG_ID'] = org_id
-        else:
-            os.environ['ORG_ID'] = input_org_id
-    if 'AWS_ACCESS_KEY_ID' not in os.environ:
-        os.environ['AWS_ACCESS_KEY_ID'] = aws_access_key_id or getpass('Input AWS_ACCESS_KEY_ID here:')
-    if 'AWS_SECRET_ACCESS_KEY' not in os.environ:
-        os.environ['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key or getpass('Input SECRET_ACCESS_KEY here:')
-    if 'AWS_DEFAULT_REGION' not in os.environ:
-        os.environ['AWS_DEFAULT_REGION'] = aws_default_region or getpass('Input AWS_DEFAULT_REGION here:')
+        credentials_string = getpass('Input JSON string containing your credentials:')
+        credentials = json.loads(credentials_string)
 
-    boto3_session = boto3.Session(
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        region_name=os.environ['AWS_DEFAULT_REGION']
-    )
+        try:
+            os.environ['ORG_ID'] = credentials['org_id']
+            os.environ['AWS_ACCESS_KEY_ID'] = credentials['aws_access_key_id']
+            os.environ['AWS_SECRET_ACCESS_KEY'] = credentials['aws_secret_access_key']
+            os.environ['AWS_SESSION_TOKEN'] = credentials['aws_session_token']
+            os.environ['AWS_DEFAULT_REGION'] = credentials['aws_default_region']
+        except KeyError as e:
+            raise KeyError(f'Credentials string missing key: {e}')
+
+    session_credentials = {
+        "aws_access_key_id": os.environ['AWS_ACCESS_KEY_ID'],
+        "aws_secret_access_key": os.environ['AWS_SECRET_ACCESS_KEY'],
+        "region_name": os.environ['AWS_DEFAULT_REGION'],
+    }
+
+    if 'AWS_SESSION_TOKEN' in os.environ:
+        session_credentials['aws_session_token'] = os.environ['AWS_SESSION_TOKEN']
+
+    boto3_session = boto3.Session(**session_credentials)
 
     print('Org ID:')
     print(os.environ['ORG_ID'])
